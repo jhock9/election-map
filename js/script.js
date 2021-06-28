709136d7cf9a8ef9ff3650a5544af75a579444a0
jhock9 <jon.hocker@gmail.com>
2021-06-28 18:57:07 -0500

    connected state colors and winners

--- js/script.js
+++ js/script.js
@@ -22,8 +22,6 @@ const createCandidate = function (name, color) {
 const blueCandidate = createCandidate("Ronald McDonald", [51, 102, 255]);
 const redCandidate = createCandidate("Bozo the Clown", [178,34,34]);
 
-
-
 // tallying the votes from .csv file
 blueCandidate.electionResults = [4,2,4,4,22,3,3,1,2,15,8,1,3,9,0,6,1,5,5,1,3,7,8,1,3,3,1,3,2,2,6,2,14,0,1,6,7,3,7,3,6,1,3,17,3,1,2,11,2,3,1];
 redCandidate.electionResults = [5,1,7,2,33,6,4,2,1,14,8,3,1,11,11,0,5,3,3,3,7,4,8,9,3,7,2,2,4,2,8,3,15,15,2,12,0,4,13,1,3,2,8,21,3,2,11,1,3,7,2];
@@ -35,6 +33,29 @@ blueCandidate.electionResults.splice(4,1,38);
 redCandidate.electionResults.splice(4,1,17);
 blueCandidate.electionResults.splice(43,1,27);
 redCandidate.electionResults.splice(43,1,11);
+
+// getting state vote count using map.js code
+const setStateResults = function (state) {
+  theStates[state].winner = null;
+  
+  if (blueCandidate.electionResults[state] > redCandidate.electionResults[state]) {
+    theStates[state].winner = blueCandidate;
+    console.log(`${blueCandidate.name} has won the state of ${theStates[state].nameFull} with a total of ${blueCandidate.electionResults[state]} votes!`)
+  } else if (blueCandidate.totalVotes < redCandidate.totalVotes) {
+    theStates[state].winner = redCandidate;
+    console.log(`${redCandidate.name} has won the state of ${theStates[state].nameFull} with a total of ${redCandidate.electionResults[state]} votes!`)
+  } else {
+    console.log(`Both candidates received ${blueCandidate.totalVotes} votes! Time for a recount!`)
+  };
+
+  const stateWinner = theStates[state].winner;
+
+  if (stateWinner !== null) {
+    theStates[state].rgbColor = stateWinner.color;
+  } else {
+    theStates[state].rgbColor = [153, 0, 255];
+  };
+};
 
 blueCandidate.totalVotes();
 redCandidate.totalVotes();
