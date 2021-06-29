7f9f2f5ba88afa0d1a083866ebdb8f97e7fcb386
jhock9 <jon.hocker@gmail.com>
2021-06-29 14:03:15 -0500

    trying to debug

--- js/script.js
+++ js/script.js
@@ -3,26 +3,27 @@ const createCandidate = function (name, color) {
     name: name,
     color: color,
     electionResults: null,
-    totalVotes: 0,
-    totalVotes: function () {
+    // totalVotes: 0,
+    
+    //methods for tallying votes//
+    totalVotes: function() {
       this.totalVotes = 0;
       
       for (let i = 0; i < this.electionResults.length; i++) {
-        this.totalVotes = this.totalVotes + this.electionResults[i];
-        // console.log(this.totalVotes);
+        this.totalVotes = this.totalVotes + this.electionResults[i]; 
       };
     }
   };
   return candidate;
 };
 
-let winner = "";
+// let winner = "";
 
 // declaring the candidates
 const blueCandidate = createCandidate("Ronald McDonald", [51, 102, 255]);
 const redCandidate = createCandidate("Bozo the Clown", [178,34,34]);
 
-// tallying the votes from .csv file
+// declaring votes from states via .csv file
 blueCandidate.electionResults = [4,2,4,4,22,3,3,1,2,15,8,1,3,9,0,6,1,5,5,1,3,7,8,1,3,3,1,3,2,2,6,2,14,0,1,6,7,3,7,3,6,1,3,17,3,1,2,11,2,3,1];
 redCandidate.electionResults = [5,1,7,2,33,6,4,2,1,14,8,3,1,11,11,0,5,3,3,3,7,4,8,9,3,7,2,2,4,2,8,3,15,15,2,12,0,4,13,1,3,2,8,21,3,2,11,1,3,7,2];
 
@@ -34,21 +35,18 @@ redCandidate.electionResults.splice(4,1,17);
 blueCandidate.electionResults.splice(43,1,27);
 redCandidate.electionResults.splice(43,1,11);
 
-blueCandidate.totalVotes();
-redCandidate.totalVotes();
-
 // getting state vote count using map.js code
 const setStateResults = function (state) {
   theStates[state].winner = null;
   
   if (blueCandidate.electionResults[state] > redCandidate.electionResults[state]) {
     theStates[state].winner = blueCandidate;
-    console.log(`${blueCandidate.name} has won the state of ${theStates[state].nameFull} with a total of ${blueCandidate.electionResults[state]} votes!`)
+    // console.log(`${blueCandidate.name} has won the state of ${theStates[state].nameFull} with a total of ${blueCandidate.electionResults[state]} votes!`)
   } else if (blueCandidate.electionResults[state] < redCandidate.electionResults[state]) {
     theStates[state].winner = redCandidate;
-    console.log(`${redCandidate.name} has won the state of ${theStates[state].nameFull} with a total of ${redCandidate.electionResults[state]} votes!`)
-  } else {
-    console.log(`Both candidates received ${blueCandidate.electionResults[state]} votes! Time for a recount!`)
+    // console.log(`${redCandidate.name} has won the state of ${theStates[state].nameFull} with a total of ${redCandidate.electionResults[state]} votes!`)
+  // } else {
+    // console.log(`Both candidates received ${blueCandidate.electionResults[state]} votes! Time for a recount!`)
   };
 
   const stateWinner = theStates[state].winner;
@@ -60,43 +58,79 @@ const setStateResults = function (state) {
   };
 };
 
-const table = document.getElementById("countryResults");
-table.children[0].children[0].children[0].innerText = redCandidate.name;
-table.children[0].children[0].children[1].innerText = redCandidate.totalVotes;
-table.children[0].children[0].children[2].innerText = blueCandidate.name;
-table.children[0].children[0].children[3].innerText = blueCandidate.totalVotes;
-table.children[0].children[0].children[5].innerText = winner;
+// adding state votes to dynamic state table
+const stateInfoTable = document.getElementById("stateResults");
+const stateHeader = stateInfoTable.children[0];
+const stateBody = stateInfoTable.children[1];
+const stateName = stateHeader.children[0].children[0];
+const stateAbbrev = stateHeader.children[0].children[1];
+const candidate1Name = stateBody.children[0].children[0];
+const candidate1Results = stateBody.children[0].children[1];
+const candidate2Name = stateBody.children[1].children[0];
+const candidate2Results = stateBody.children[1].children[1];
+const winnerName = stateBody.children[2].children[1];
 
-/* testing code with new syntax
-let countryResultsName1 = document.querySelector(".country-results .name-1");
-let countryResultsVotes1 = document.querySelector(".country-results .results-1");
-let countryResultsName2 = document.querySelector(".country-results .name-2");
-let countryResultsVotes2 = document.querySelector(".country-results .results-2");
-let countryResultsWinner = document.querySelector(".country-results .winner");
+stateName.innerText = theStates[state].nameFull;
+stateAbbrev.innerText = theStates[state].nameAbbrev;
+candidate1Name.innerText = redCandidate.name;
+candidate1Results.innerText = redeCandidate.electionResults[state];
+candidate2Name.innerText = blueCandidate.name;
+candidate2Results.innerText = blueCandidate.electionResults[state];
 
-countryResultsName1 = redCandidate.name;
-countryResultsVotes1 = redCandidate.totalVotes;
-countryResultsName2 = blueCandidate.name;
-countryResultsVotes2 = blueCandidate.totalVotes;
-countryResultsWinner = winner;
+if (stateWinner !== null) {
+  winnerName.innerText = "TIE";
+} else {
+  winnerName.innerText = stateWinner.name;
+};
+
+/*
+// adding up state's results by candidate
+blueCandidate.totalVotes = function () {
+  this.totalVotes = 0;
+  
+  for (let i = 0; i < this.electionResults.length; i++) {
+    this.totalVotes = this.totalVotes + this.electionResults[i];
+    // console.log(this.totalVotes);
+  };
+}
+
+redCandidate.totalVotes = function () {
+  this.totalVotes = 0;
+  
+  for (let i = 0; i < this.electionResults.length; i++) {
+    this.totalVotes = this.totalVotes + this.electionResults[i];
+    // console.log(this.totalVotes);
+  };
+}
 */
 
-
+// blueCandidate.totalVotes();
+// redCandidate.totalVotes();
 
 //announcing results
-const electionResults = function () {
+// const electionResults = function () {
   if (blueCandidate.totalVotes > redCandidate.totalVotes) {
     let winner = blueCandidate.name;
-    console.log(`${blueCandidate.name} has won the election with a total of ${blueCandidate.totalVotes} votes!`)
+    // console.log(`${blueCandidate.name} has won the election with a total of ${blueCandidate.totalVotes} votes!`)
   } else if (blueCandidate.totalVotes < redCandidate.totalVotes) {
     let winner = redCandidate.name;
-    console.log(`${redCandidate.name} has won the election with a total of ${redCandidate.totalVotes} votes!`)
+    // console.log(`${redCandidate.name} has won the election with a total of ${redCandidate.totalVotes} votes!`)
   } else {
-    console.log(`Both candidates received ${blueCandidate.totalVotes} votes! Time for a recount!`)
+    let winner = "It's a tie!";
+    // console.log(`Both candidates received ${blueCandidate.totalVotes} votes! Time for a recount!`)
   };
-};
+// };
 
 electionResults();
 
-console.log(blueCandidate);
-console.log(redCandidate);
+// adding country votes to header table
+const countryInfoTable = document.getElementById("countryResults");
+const countryHeader = countryInfoTable.children[0].children[0];
+countryHeader.children[0].innerText = redCandidate.name;
+countryHeader.children[1].innerText = redCandidate.totalVotes;
+countryHeader.children[2].innerText = blueCandidate.name;
+countryHeader.children[3].innerText = blueCandidate.totalVotes;
+countryHeader.children[5].innerText = winner;
+
+// console.log(blueCandidate);
+// console.log(redCandidate);
